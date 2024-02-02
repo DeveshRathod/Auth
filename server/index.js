899eb79af22cd4c9ebfec5451668131cf2bf8035
Devesh Rathod <108536407+DeveshRathod@users.noreply.github.com>
2024-02-02 21:05:13 +0530

    first

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("DB Connected");
+});
+
+const app = express();
+
+app.listen(3001, () => {
+  console.log("Server Started!!!");
+});
